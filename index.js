309509fed2377d25d4666613f5eb762bbdc403c8
Hyeongjin3581 <poll3581@naver.com>
2026-07-28 18:09:11 +0900

    18:09

--- index.js
+++ index.js
@@ -34,13 +34,35 @@ let memberTable = [
 ]
 // 6.  JS 메모리 샘플 데이터
 
-// 커뮤니티 기능
-function 게시글작성(){}
+// 상단 메뉴 클릭 시 각 카테고리/페이지로 이동
+function 연초페이지이동(카테고리) {}
+function 릴페이지이동(카테고리) {}
+function 아이코스페이지이동(카테고리) {}
+function 담배브랜드페이지이동() {}
+function 커뮤니티페이지이동() {}
 
-function 게시글삭제(){}
+// 카테고리별 제품 조회 및 필터링
+function 제품목록조회(카테고리) {}
+function 제품상세보기(제품ID) {}
+function 제품별점조회(제품ID) {}
+function 제품검색(검색어) {}
 
-function 게시글수정(){}
+// 커뮤페이지 
+function 오늘의핫글(게시글ID){}
+function 게시물보기(게시글ID){}
+function 게시물작성(게시글ID) {}
+function 게시물삭제(회원고유ID) {}
+function 게시물수정(회원고유ID) {}
 
-function 추천증가(){}
+// 커뮤상세 페이지 투표 반응
+function 추천(게시글ID) {}
+function 비추천(게시글ID) {}
+function 댓글남기기(회원고유ID){}
 
-function 추천감소(){}
+// 담배브랜드 페이지
+
+function 브랜드목록조회(브랜드ID){}
+function 인기브랜드목록(담배ID){}
+
+function 초성필터링(){}
+function 초성별제품조회(){}
