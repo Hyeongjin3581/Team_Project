3061963b64a6f57265114a2e6a903f473e2bbaa5
yoonsung157 <yoonsung157@gmail.com>
2026-07-29 17:21:49 +0900

    랭킹

--- 연초.js
+++ 연초.js
@@ -1,9 +1,33 @@
 function entireClick() {
     document.querySelector('.cigarRank').style.display = 'none';
     document.querySelector('.entireRank').style.display = 'block';
-}
+
+    document.querySelector('.entireBtn').style.borderBottom = '2px solid black'
+    document.querySelector('.cigarBtn1').style.borderBottom = 'none'
+    document.querySelector('.cigarBtn2').style.borderBottom = 'none'
+    document.querySelector('.btn3').style.borderBottom = 'none'
+}   
 
 function click2() {
     document.querySelector('.entireRank').style.display = 'none';
     document.querySelector('.cigarRank').style.display = 'block';
+
+    document.querySelector('.entireBtn').style.borderBottom = 'none'
+    document.querySelector('.cigarBtn1').style.borderBottom = '2px solid black'
+    document.querySelector('.cigarBtn2').style.borderBottom = 'none'
+    document.querySelector('.btn3').style.borderBottom = 'none'
+}
+
+function click3() {
+    document.querySelector('.entireBtn').style.borderBottom = 'none'
+    document.querySelector('.cigarBtn1').style.borderBottom = 'none'
+    document.querySelector('.cigarBtn2').style.borderBottom = '2px solid black'
+    document.querySelector('.btn3').style.borderBottom = 'none'
+}
+
+function click4() {
+    document.querySelector('.entireBtn').style.borderBottom = 'none'
+    document.querySelector('.cigarBtn1').style.borderBottom = 'none'
+    document.querySelector('.cigarBtn2').style.borderBottom = 'none'
+    document.querySelector('.btn3').style.borderBottom = '2px solid black'
 }
